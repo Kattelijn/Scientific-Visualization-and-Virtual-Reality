09b1dcc7ca33da2a0482828a6d6d163177123ec1
Kattelijn <78729121+Kattelijn@users.noreply.github.com>
2024-11-05 19:56:18 +0100

    final visualization

--- plotly3d.py
+++ plotly3d.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 df = pd.read_csv('cars.csv')
+
+df["year"] = "19" + df["year"].astype(str)
+
+size_factor = 2
 
 fig = px.scatter_3d(
     df,
@@ -18,13 +23,25 @@ fig = px.scatter_3d(
     range_z=[0, df["weigth"].max() + 500]
 )
 
+for trace in fig.data:
+    trace.marker.line = dict(color='black')
+
+static_trace = go.Scatter3d(
+    x=df["MPG"],
+    y=df["horsepower"],
+    z=df["weigth"],
+    mode='markers',
+    marker=dict(
+        size=df["cylinders"] * size_factor,
+        color='gray',
+        opacity=0.1
+    ),
+    hoverinfo="skip",
+    showlegend=False
+)
+
+fig.add_trace(static_trace)
+
 fig.show()
 
 fig.write_html("car_data_3D_animation.html")
-
-
-# TODO
-# change years 70 -> 1970
-# change representation of cylinders
-# add background of all data
-# buttons to select origin/cylinders
